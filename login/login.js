5b41c48895d304ca59059776651ca9da554faf6d
Franco Faccelli <56889679+franfy@users.noreply.github.com>
2026-09-10 19:43:58 -0300

    Add files via upload

--- login/login.js
+++ login/login.js
@@ -1,48 +1,11 @@
-const formularioLogin = document.getElementById("formularioLogin");
-const formularioSignIn = document.getElementById("divSignIn");
-const inputEmail = document.getElementById("inputEmail");
-const inputPass = document.getElementById("inputPass");
-const btnLogIn = document.getElementById("btnLogIn");
-const btnSignIn = document.getElementById("btnSignIn");
-const resultadoLogin = document.getElementById("resultadoLogin");
+const formularioLogin = document.getElementById("containerLogin");
+const iframeSignin = document.getElementById("iframeSignin");
 
-mostrar(formularioLogin, "flex");
-ocultar(resultadoLogin);
+const btnSignin = document.getElementById("btnSignin");
 
-btnLogIn.addEventListener("click", () => {
-    tomaDeDatosLogin();
+formularioLogin.style.display = "block";
+
+btnSignin.addEventListener( "click", () => {
+    formularioLogin.style.display = "none";
+    iframeSignin.style.display = "block";
 });
-
-btnSignIn.addEventListener("click", () => {
-    ocultar(formularioLogin);
-    cargarSignIn(formularioSignIn);
-    mostrar(formularioSignIn, "block");
-});
-
-function ocultar(contenido) {contenido.style.display = "none"}
-
-function mostrar(contenido, forma) {contenido.style.display = forma;}
-
-function tomaDeDatosLogin(){
-    let email = inputEmail.value;
-    let pass = inputPass.value;
-    
-    if (email === "" && pass === ""){
-        mostrar(resultadoLogin, "block");
-        resultadoLogin.style.color = "var(--rojo-warning-texto)";
-        inputEmail.style.borderColor = "var(--rojo-warning-input)";
-        inputPass.style.borderColor = "var(--rojo-warning-input)";
-    } else {
-        alert("Gracias por iniciar sesion...");
-    }
-}
-
-function cargarSignIn(contenido){
-    fetch('signIn/signIn.html')
-        .then(function(respuesta){
-            return respuesta.text();
-        })
-        .then(function(html){
-            contenido.innerHTML = html;
-        })
-}
